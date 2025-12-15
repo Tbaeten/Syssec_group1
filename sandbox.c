9fa0d353e15bcbf39b2d38fa5cad6d580e905674
Tbaeten <baeten.thomas2@gmail.com>
2025-12-15 12:00:51 +0100

    Sygnal handling

--- sandbox.c
+++ sandbox.c
@@ -7,6 +7,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#ifndef PR_SET_SYSCALL_USER_DISPATCH
+# define PR_SET_SYSCALL_USER_DISPATCH 59
+# define PR_SYS_DISPATCH_OFF 0
+# define PR_SYS_DISPATCH_ON 1
+# define SYSCALL_DISPATCH_FILTER_ALLOW 0
+# define SYSCALL_DISPATCH_FILTER_BLOCK 1
+#endif
+
 // 1. The Trampoline
 // This global variable allows us to identify the memory range of this function
 extern void syscall_trampoline(void);
@@ -26,9 +34,10 @@ int is_syscall_allowed(int syscall_nr) {
 // 3. The Signal Handler
 void sigsys_handler(int sig, siginfo_t *info, void *ctx) {
     ucontext_t *uc = (ucontext_t *)ctx;
+    greg_t *regs = uc->uc_mcontext.gregs;
     
     // Get the syscall number (RAX holds the syscall number on x86_64)
-    int syscall_nr = uc->uc_mcontext.gregs[REG_RAX];
+    int syscall_nr = (int)regs[REG_RAX];
 
     if (!is_syscall_allowed(syscall_nr)) {
         const char *msg = "Sandbox violation detected!\n";
@@ -36,14 +45,18 @@ void sigsys_handler(int sig, siginfo_t *info, void *ctx) {
         exit(1);
     }
 
-    // Prepare to redirect to trampoline
+    // Calculates where to return which is 2 bytes from the syscall
+    greg_t return_addr = regs[REG_RIP] + 2;
     
-    // A. Save the original return address (RIP + 2 bytes for 'syscall' instr)
-    // We put it in R15 so the trampoline knows where to return.
-    uc->uc_mcontext.gregs[REG_R15] = uc->uc_mcontext.gregs[REG_RIP] + 2;
+    // simulates push onto user stack which does decrement the stack pointer by 8 bytes
+    regs[REG_RSP] -= 8;
 
-    // B. Hijack the Instruction Pointer (RIP) to point to our trampoline
-    uc->uc_mcontext.gregs[REG_RIP] = (greg_t)&syscall_trampoline;
+    // writes the return address into the memory at new stack pointer position
+    *(uintptr_t *)regs[REG_RSP] = return_addr;
+
+    // Hijacks instruction pointer to execute trampoline 
+    regs[REG_RIP] = (greg_t)&syscall_trampoline;
+
 }
 
 // 4. The Setup
@@ -52,10 +65,16 @@ void init_sandbox() {
     // A. Load Policy File ...
     
     // B. Setup Signal Handler
+
     struct sigaction sa = {0};
     sa.sa_sigaction = sigsys_handler;
-    sa.sa_flags = SA_SIGINFO | SA_NODEFER; // SA_NODEFER allows recursion if needed
-    sigaction(SIGSYS, &sa, NULL);
+    // getter of context of registers (RIP)
+    sa.sa_flags = SA_SIGINFO | SA_NODEFER;
+
+    if (sigaction(SIGSYS, &sa, NULL) < 0) {
+        perror("sigaction failed");
+        exit(1);
+    }
 
     // C. Enable Syscall User Dispatch
     // We set the allowed region to ONLY be the trampoline function.
